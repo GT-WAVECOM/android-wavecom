065a75f106ece2fb09f2c4cc8997d738cfb209c5
xwang875 <xcwang821@gatech.edu>
2019-09-23 22:06:32 -0400

    init the project

---
+++ app/src/main/java/edu/gatech/xcwang/wavecom/MainActivity.java
@@ -0,0 +1,13 @@
+package edu.gatech.xcwang.wavecom;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
